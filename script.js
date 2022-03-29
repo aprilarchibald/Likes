f80478157a1ef752f81050a3f5bb2a13534f368c
April Archibald <aprilsclearskies@gmail.com>
2022-03-29 16:45:35 -0700

    commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Part 1
+// let count=3
+// let countElement = document.querySelector(".count")
+// function increaseLikes() {
+//     count++;
+//     // console.log(count)
+//     countElement.innerText=count + " like(s)"
+// }
+
+// Part 2
+let count1=9
+let countElement1 = document.querySelector(".count1")
+function increaseLikes1(){
+    count1++;
+    countElement1.innerText =count1 + " like(s)"
+}
+
+let count2=12
+let countElement2 = document.querySelector(".count2")
+function increaseLikes2(){
+    count2++;
+    countElement2.innerText =count2 + " like(s)"
+}
+
+let count3=9
+let countElement3 = document.querySelector(".count3")
+function increaseLikes3(){
+    count3++;
+    countElement3.innerText =count3 + " like(s)"
+}
